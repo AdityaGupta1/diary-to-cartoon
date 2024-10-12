e62c81ecdf18be65a7198fa348ca90b181dc5b15
AdityaGupta1 <adityag@gmail.com>
2024-10-12 17:18:48 -0400

    extravagant indeed, my good sir

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import gradio as gr
+import google.generativeai as genai
+import env
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+genai.configure(api_key=env.GEMINI_API_KEY)
+gemini_model = genai.GenerativeModel('gemini-1.5-flash')
+
+def greet(name, extravagance):
+    prompt = f'Greet someone named ${name}. On a scale of 0 to 10, where 0 is the most straightforward and 10 is the most extravagant, you should be at a ${extravagance}.'
+    response = gemini_model.generate_content(prompt)
+    return response.text
 
 demo = gr.Interface(
     fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
+    inputs=[gr.Textbox(), gr.Slider(minimum=0, maximum=10, step=1)],
+    outputs=[gr.TextArea()],
 )
 
 demo.launch()
