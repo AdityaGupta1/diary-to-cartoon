6e70d4ed3e9db09a26f62ed46164b098a86d0c2e
AdityaGupta1 <adityag@gmail.com>
2024-10-12 17:03:00 -0400

    initial commit 2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import gradio as gr
 
-def greet(name):
-    return f"Hello {name}!"
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
