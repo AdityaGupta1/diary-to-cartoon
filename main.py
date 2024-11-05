9f71d7f69b4a0d2710335e4716c067189ab38575
AdityaGupta1 <adityag@gmail.com>
2024-11-05 18:35:06 -0500

    use gemini to pick between multiple variations

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from diffusers import AutoPipelineForText2Image
 import torch
 from PIL import Image
 
-num_panels = 4
-
 genai.configure(api_key=env.GEMINI_API_KEY)
 gemini_model = genai.GenerativeModel('gemini-1.5-flash')
 
@@ -32,7 +30,7 @@ Remove any references to copyrighted material and replace them with some generic
 Do not include any extra text, as your response will be used verbatim in a Python program for further processing.
     '''
     response = gemini_model.generate_content([gemini_prompt, diary_image])
-    diary_contents = response.text
+    diary_contents = response.text.strip()
 
     progress((current_gemini_step, num_gemini_steps), 'generating prompts v1...')
     current_gemini_step += 1
@@ -58,7 +56,7 @@ Include the author's color or race as well as other distinguishing characteristi
 Prompts should be written in the third person.
 Prompts should be one sentence at most.
 
-Here is an example:
+Here is an example of good output:
 
 A Polynesian man with short black hair, a green tank top, and glasses eating waffles and drinking orange juice at a table.
 -----
@@ -75,71 +73,75 @@ Assume that the four prompts will be used in four separate contexts.
 Do not include any extra text as your response will be used as a string in a Python program and it needs to be concise.
     '''
     response = gemini_model.generate_content([gemini_prompt, author_image])
-    prompts_v1 = response.text
+    prompts_v1 = response.text.strip()
 
-    progress((current_gemini_step, num_gemini_steps), 'generating prompts v2...')
-    current_gemini_step += 1
+    image_prompts = prompts_v1.split('\n-----\n')
 
-    gemini_prompt = f'''
-Here are some prompts for use with an image generation tool:
+    def prompt_editor(prompt):
+        return "A comic book panel of " + prompt[0:1].lower() + prompt[1:]
 
-{prompts_v1}
+    modified_image_prompts = [prompt_editor(prompt) for prompt in image_prompts]
 
-Rewrite the prompts to ensure that each one contains an independent description of the main subject. Additionally, for any recurring characters, make up some description of them and use that same description in each prompt in which that character appears.
-
-For example:
-
-A corgi with white and brown fur eating waffles and drinking orange juice at a table.
------
-The same corgi sitting in a lecture hall, learning about macroeconomics.
------
-The same corgi at a birthday party with his friends, blowing out the candles on a cake.
------
-The same corgi getting drunk with his friends at a karaoke bar.
-
-should turn into:
-
-A corgi with white and brown fur eating waffles and drinking orange juice at a table.
------
-A corgi with white and brown fur sitting in a lecture hall, learning about macroeconomics.
------
-A corgi with white and brown fur at a birthday party with an orange tabby cat and a green turtle, blowing out the candles on a cake.
------
-A corgi with white and brown fur getting drunk with an orange tabby cat and a green turtle at a karaoke bar.
-
-Keep the same format as the input prompts. That is, use ----- to separate the prompts and keep prompts concise.
-Prompts should be at most 1 sentence each.
-
-Do not include any extra text as your response will be used as a string in a Python program and it needs to be concise.
-    '''
-    response = gemini_model.generate_content(gemini_prompt)
-    prompts_v2 = response.text
-
-    progress2 = gr.Progress()
-
-    image_prompts = prompts_v2.split('\n-----\n')
-
-    image_prompt_suffix = 'cartoon style, soft rounded edges, expressive characters, simple shading'
+    num_panels = 4
+    num_variations_per_panel = 2
 
     images = []
+    for panel_idx in range(num_panels):
+        for variation_idx in range(num_variations_per_panel):
+            progress((panel_idx * num_variations_per_panel + variation_idx, num_panels * num_variations_per_panel), 'generating images...')
+            image = pipe(prompt=modified_image_prompts[panel_idx], num_inference_steps=2, guidance_scale=0.0).images[0]
+            images.append(image)
+
+    image_width, image_height = images[0].size
+
+    variations_image_width = image_width * num_variations_per_panel
+    variations_image_height = image_height * num_panels
+    variations_image = Image.new('RGB', (variations_image_width, variations_image_height))
+    for x in range(num_variations_per_panel):
+        for y in range(num_panels):
+            variations_image.paste(images[y * num_variations_per_panel + x], (x * image_width, y * image_height))
+
+    progress((current_gemini_step, num_gemini_steps), 'selecting panels...')
+    current_gemini_step += 1
+
+    example_variation_choices = ""
+    example_variation_idx = 0
     for i in range(num_panels):
-        progress2((i, num_panels), 'generating images...')
-        modified_image_prompt = image_prompts[i] + ' ' + image_prompt_suffix
-        image = pipe(prompt=modified_image_prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
-        images.append(image)
+        example_variation_choices += str(example_variation_idx) + ", "
+        example_variation_idx = (example_variation_idx + 1) % num_variations_per_panel
+    example_variation_choices = example_variation_choices[:-2]
 
-    width, height = images[0].size
+    gemini_prompt = f'''
+Attached is a grid of AI-generated images, with {num_panels} rows and {num_variations_per_panel} columns. Each row contains {num_variations_per_panel} variations of a single panel of a {num_panels}-panel comic. The story of the comic is the following:
 
-    new_width = width * 2
-    new_height = height * 2
-    new_image = Image.new('RGB', (new_width, new_height))
+{diary_contents}
 
-    new_image.paste(images[0], (0, 0))
-    new_image.paste(images[1], (width, 0))
-    new_image.paste(images[2], (0, height))
-    new_image.paste(images[3], (width, height))
+Pick the best combination of {num_panels} panels such that there is exactly one in each category. When choosing panels, take the following into account:
+- Image quality (e.g. don't choose images with extra limbs or lots of noise)
+- Narrative progression
+- Consistent art style
 
-    return new_image, prompts_v2, prompts_v1, diary_contents
+Output your choices in a comma-delimited list, with 0 being the left-most image for that row and {num_variations_per_panel - 1} being the right-most image for that row. Do not include any extra text as your response will be used as a string in a Python program and it needs to be concise.
+    '''
+    response = gemini_model.generate_content([gemini_prompt, variations_image])
+    variation_selections_text = response.text.strip()
+    variation_selections = [int(idx) for idx in variation_selections_text.split(",")]
+
+    comic_width = image_width * 2
+    comic_height = image_height * 2
+    comic_image = Image.new('RGB', (comic_width, comic_height))
+
+    panel_indices = []
+    for i in range(num_panels):
+        panel_indices.append(i * num_variations_per_panel + variation_selections[i])
+
+    comic_image.paste(images[panel_indices[0]], (0, 0))
+    comic_image.paste(images[panel_indices[1]], (image_width, 0))
+    comic_image.paste(images[panel_indices[2]], (0, image_height))
+    comic_image.paste(images[panel_indices[3]], (image_width, image_height))
+
+    modified_image_prompts_text = "\n\n".join(modified_image_prompts)
+    return comic_image, variation_selections, variations_image, modified_image_prompts_text, diary_contents
 
 demo = gr.Interface(
     fn = describe_image,
@@ -149,8 +151,9 @@ demo = gr.Interface(
     ],
     outputs = [
         gr.Image(type='pil', label='comic'),
-        gr.TextArea(label='prompts v2 (debug)'),
-        gr.TextArea(label='prompts v1 (debug)'),
+        gr.TextArea(label='variation selections (debug)'),
+        gr.Image(type='pil', label='variations (debug)'),
+        gr.TextArea(label='modified image prompts (debug)'),
         gr.TextArea(label='diary contents')
     ],
     flagging_mode = 'never'
